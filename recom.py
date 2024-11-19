25ced1fc9321e16bc156da45a89ab99203dabfbe
ProgramJakim <jakimlopez28@gmail.com>
2024-11-20 04:57:41 +0800

    new message

--- recom.py
+++ recom.py
@@ -1,9 +1,7 @@
 import mysql.connector
 import networkx as nx
 
-# linisin ko lang muna
-# holy cow what is happening to the world!
-# try recom2, kung lalabas
+# Hello Everyone!
     
 # Connect to MySQL database
 db_connection = mysql.connector.connect(
